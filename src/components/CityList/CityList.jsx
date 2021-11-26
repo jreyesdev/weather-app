7e0a4f4df26067352f989565f602c384fc15e3f9
jreyesdev <djrm1204@gmail.com>
2021-11-26 01:32:02 -0400

    Grid

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Grid from "@mui/material/Grid";
 import CityInfo from "../CityInfo/CityInfo";
 import Weather from "../Weather/Weather";
 
@@ -8,8 +9,14 @@ const CityList = ({ cities }) => {
     <ul>
       {cities.map((e, i) => (
         <li key={i}>
-          <CityInfo city={e.city} country={e.country} />
-          <Weather temp={e.temp} icon="sunny" />
+          <Grid container justifyContent="center" alignItems="center">
+            <Grid item xs={12} md={8}>
+              <CityInfo city={e.city} country={e.country} />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Weather temp={e.temp} icon="sunny" />
+            </Grid>
+          </Grid>
         </li>
       ))}
     </ul>
