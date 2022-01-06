8d46d1085f44f8deff13448741377fa33ee5b28f
jreyesdev <djrm1204@gmail.com>
2022-01-06 19:56:18 -0400

    Pagina welcome

---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const WelcomePage = (props) => {
+  return <div>Welcome page</div>;
+};
+
+WelcomePage.propTypes = {};
+
+export default WelcomePage;
