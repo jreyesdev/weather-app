1469b26926f4f51b76a5b5e9575a116bcdb9b376
jreyesdev <djrm1204@gmail.com>
2022-01-06 19:55:55 -0400

    Pagina de ciudad

---
+++ src/pages/CityPage.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Grid from "@mui/material/Grid";
+import CityInfo from "../components/CityInfo/CityInfo";
+import Weather from "../components/Weather/Weather";
+import WeatherDetails from "../components/WeatherDetails/WeatherDetails";
+import ForeCastChart from "../components/ForeCastChart/ForeCastChart";
+import ForeCast from "../components/ForeCast/ForeCast";
+
+const ITEMS_FORE = [
+  { hour: 18, state: "sunny", temperature: 23, weekDay: "Jueves" },
+  { hour: 6, state: "cloud", temperature: 17, weekDay: "Viernes" },
+  { hour: 12, state: "fog", temperature: 18, weekDay: "Viernes" },
+  { hour: 18, state: "cloudy", temperature: 19, weekDay: "Viernes" },
+  { hour: 14, state: "rain", temperature: 17, weekDay: "Sábado" },
+  { hour: 18, state: "rain", temperature: 17, weekDay: "Sábado" },
+];
+
+const data = [
+  { dayHour: "Jue 18", min: 14, max: 22 },
+  { dayHour: "Vie 06", min: 18, max: 27 },
+  { dayHour: "Vie 12", min: 18, max: 28 },
+  { dayHour: "Vie 18", min: 18, max: 25 },
+  { dayHour: "Sab 06", min: 15, max: 22 },
+  { dayHour: "Sab 12", min: 12, max: 19 },
+];
+
+const CityPage = () => {
+  return (
+    <Grid container justifyContent="center" direction="column">
+      <Grid item xs={12}>
+        <CityInfo city="Caracas" country="Venezuela" />
+      </Grid>
+      <Grid container item xs={12}>
+        <Grid item xs={8}>
+          <Weather icon="cloudy" temp={20} />
+        </Grid>
+        <Grid item xs={4}>
+          <WeatherDetails humidity={80} wind={9} />
+        </Grid>
+      </Grid>
+      <Grid item>
+        <ForeCastChart data={data} />
+      </Grid>
+      <Grid item>
+        <ForeCast itemList={ITEMS_FORE} />
+      </Grid>
+    </Grid>
+  );
+};
+
+CityPage.propTypes = {};
+
+export default CityPage;
