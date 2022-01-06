2650668787faa4549ccc90850cb3d025e41d4346
jreyesdev <djrm1204@gmail.com>
2022-01-06 18:29:10 -0400

    Componente de graficas de linea

---
+++ src/components/ForeCastChart/ForeCastChart.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const ForeCastChart = ({ data }) => {
+  return (
+    <>
+      <LineChart
+        height={250}
+        width={700}
+        margin={{ top: 20, left: 5, bottom: 20 }}
+        data={data}
+      >
+        <XAxis dataKey="dayHour" />
+        <YAxis />
+        <CartesianGrid />
+        <Tooltip />
+        <Legend />
+        <Line dataKey="max" type="monotone" stroke="#FF0000" />
+        <Line dataKey="min" type="monotone" stroke="#0000FF" />
+      </LineChart>
+    </>
+  );
+};
+
+ForeCastChart.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      dayHour: PropTypes.string.isRequired,
+      min: PropTypes.number.isRequired,
+      max: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default ForeCastChart;
---
+++ src/components/ForeCastChart/ForeCastChart.test.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ForeCastChart from "./ForeCastChart";
+
+test("ForeCastChart render", async () => {
+  //const {} = render(<ForeCastChart />);
+});
