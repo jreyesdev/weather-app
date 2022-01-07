81786a245a71dd37de16a2b29b5f47565d9b2ed9
jreyesdev <djrm1204@gmail.com>
2022-01-07 14:33:23 -0400

    Layout principal de la aplicacion

---
+++ src/components/AppFrame/AppFrame.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link as LinkRouter } from "react-router-dom";
+import { IconContext } from "react-icons";
+import Grid from "@mui/material/Grid";
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import Link from "@mui/material/Link";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { WiDaySunny } from "react-icons/wi";
+
+const AppFrame = ({ children }) => {
+  return (
+    <Grid container justifyContent="center">
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton color="inherit" aria-label="menu">
+            <Link
+              component={LinkRouter}
+              to="/main"
+              color="inherit"
+              aria-label="menu"
+            >
+              <IconContext.Provider value={{ size: "2em" }}>
+                <WiDaySunny />
+              </IconContext.Provider>
+            </Link>
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Weather App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid item xs={12} sm={11} md={10} lg={8}>
+        {children}
+      </Grid>
+    </Grid>
+  );
+};
+
+AppFrame.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppFrame;
---
+++ src/components/AppFrame/AppFrame.stories.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppFrame from "./AppFrame";
+
+export default {
+  title: "AppFrame",
+  component: AppFrame,
+};
+
+export const AppFrameExample = () => (
+  <Router>
+    <AppFrame>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus id
+      nostrum modi tenetur placeat quasi cum, nihil hic delectus, quisquam
+      dolores totam sit praesentium quidem animi maxime quam ipsum vitae? Nisi
+      rerum libero iure, exercitationem, ducimus iste itaque obcaecati quam
+      laudantium fugit sint magnam optio commodi, deserunt vero ipsam tenetur
+      facere praesentium! Placeat iusto nemo nobis incidunt ipsa similique
+      provident? Perspiciatis, molestias quis! Voluptate, neque. Cumque quod
+      esse neque iusto quidem ipsam itaque, quia, recusandae rem porro rerum
+      consequatur ullam maiores delectus omnis molestiae temporibus tempore at?
+      Possimus, odio dicta. Repudiandae harum perferendis voluptatibus inventore
+      dolores, eos fugit quia magnam nulla atque minus modi, maxime expedita
+      ullam accusantium dignissimos nemo blanditiis aperiam placeat molestiae
+      suscipit illo in alias? Sit, blanditiis! Omnis amet molestiae sunt
+      laboriosam porro doloremque temporibus maiores quaerat id eum. Eius
+      officiis atque hic consectetur reprehenderit consequuntur quo, consequatur
+      delectus, vel, amet repellat minima voluptates accusamus accusantium aut.
+      Unde dignissimos neque tempore pariatur, assumenda quo minima architecto
+      voluptates fuga reiciendis modi accusantium quas praesentium,
+      exercitationem nemo impedit quae quibusdam, distinctio a itaque corporis
+      recusandae enim! Commodi, suscipit eius. Nesciunt vitae ratione error. In
+      deserunt, ullam, nostrum neque asperiores maxime quo consectetur quisquam
+      natus quasi tenetur similique vitae pariatur quas eaque amet accusamus
+      fugit repudiandae! Pariatur voluptatum nobis ipsam. Distinctio iusto,
+      molestiae voluptas aliquam labore consequuntur omnis unde id veniam, vero,
+      ex deserunt iste suscipit repellat nisi! Dignissimos autem mollitia
+      eveniet unde neque hic adipisci corporis natus officiis quod. Pariatur
+      unde fuga non ratione corporis quaerat. Consectetur, aperiam optio aut a
+      delectus laborum? Delectus, soluta! Provident voluptatum dolore aperiam
+      animi quas nobis vel fuga ipsa nemo natus? Eligendi, similique? Aliquid at
+      minus nam voluptas quis. Obcaecati, quibusdam possimus. In consequatur
+      deserunt autem doloremque architecto facilis officia expedita
+      necessitatibus praesentium, dolor perferendis repudiandae vitae vero
+      officiis ratione quidem numquam et! Similique nobis, eveniet ratione
+      aperiam harum accusamus ipsa fugiat itaque voluptate eos quae temporibus
+      molestias dolore asperiores ab ut culpa quaerat ea ad minima! Ipsam eaque
+      quam incidunt eos neque. Esse quis veniam explicabo, harum tempore
+      corporis assumenda corrupti ipsum, eum ab beatae qui. Esse nulla molestias
+      vitae pariatur. Rerum sit fuga voluptas pariatur fugit enim deserunt
+      tempora tenetur distinctio? Iusto atque vitae nobis exercitationem odio
+      reiciendis et alias quod modi expedita aspernatur ut ipsa, sequi officia
+      eos deleniti repellat consequuntur tempora corporis amet ratione
+      voluptate? Minus sequi autem ipsum! Facere veritatis ratione consequuntur
+      quod iure, vitae eos at ut nesciunt ullam id voluptatem ea enim, nam
+      eligendi quisquam, voluptas dolores qui iusto deserunt hic quasi! Vel
+      omnis excepturi minus. Corporis nisi voluptatem totam praesentium commodi
+      dignissimos nemo ad laudantium delectus repellat, atque distinctio facere
+      dolorum ullam molestias temporibus incidunt culpa cupiditate rerum.
+      Sapiente rerum exercitationem, non nostrum optio sunt! Recusandae maxime
+      voluptates totam labore autem laborum excepturi aliquid vero libero
+      mollitia facere ex reiciendis impedit repellat, voluptate quibusdam
+      ducimus debitis, optio laudantium unde. Animi suscipit pariatur veniam
+      accusantium atque. Quod maxime officia ad libero aliquam perspiciatis
+      consequuntur! Cum vitae veniam ipsum omnis repudiandae, in iste iure
+      corporis fugiat quos dolorum illum ut sunt. Illo praesentium facilis
+      mollitia est ratione! Deserunt dolor adipisci accusamus fuga id? Neque
+      temporibus, quisquam eos nulla illum iusto placeat pariatur cumque
+      dignissimos impedit expedita voluptatem consequatur praesentium id nihil
+      quo distinctio, suscipit ducimus fugit magnam! Ex voluptatem modi
+      cupiditate quasi, doloribus vero eveniet in itaque? Quidem, omnis!
+      Perferendis nesciunt vel officia dolore unde? Reprehenderit, animi alias
+      nulla quis neque aut eaque modi praesentium corporis doloremque. Dolorem
+      sequi iste ab unde dolores! Obcaecati eos dolore voluptatem a quaerat.
+      Labore doloribus dignissimos, sunt corporis laboriosam, quae esse illum
+      similique, quas inventore ullam ab sit recusandae assumenda voluptatibus.
+    </AppFrame>
+  </Router>
+);
