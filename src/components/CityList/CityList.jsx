7acf290095fa803cebcf745f469a25464202a325
Jesus Reyes <djrm1204@gmail.com>
2022-01-23 20:00:31 -0400

    Agregado seccion para mostrar error

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import Alert from "@mui/material/Alert";
 import Grid from "@mui/material/Grid";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
@@ -13,6 +14,7 @@ import WeatherAxios from "../../api/WeatherAxios";
 const CityList = ({ cities, onClickCity }) => {
   const [allWeather, setAllWeather] = useState({});
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     cities.forEach((c, i) => {
@@ -28,44 +30,62 @@ const CityList = ({ cities, onClickCity }) => {
         },
       });
       if (resp.data.main.temp) {
-        const prop = `${i}-${country}`,
-          propValue = {};
+        const propValue = {};
         propValue.temp = Number(
           ConvertUnits(resp.data.main.temp).from("K").to("C").toFixed(0)
         );
         propValue.icon = resp.data.weather[0].main.toLowerCase();
-        setAllWeather((current) => ({ ...current, [prop]: propValue }));
+        setAllWeather((current) => ({
+          ...current,
+          [`${i}-${country}`]: propValue,
+        }));
         setLoading(false);
       }
     } catch (e) {
       console.error(e);
+      setError((currentError) => {
+        if (e.response) {
+          return "Ha ocurrido un error en el servidor del clima";
+        } else if (e.request) {
+          return "Verifica tu conexion a internet";
+        } else {
+          return "Error al cargar datos";
+        }
+      });
     }
   };
 
   return (
-    <List>
-      {cities.map((e, i) => (
-        <ListItem button key={`${i}-${e.country}`} onClick={onClickCity}>
-          <Grid container justifyContent="center" alignItems="center">
-            <Grid item xs={12} md={8}>
-              <CityInfo city={e.city} country={e.country} />
+    <>
+      {error && (
+        <Alert severity="error" onClose={() => setError(null)}>
+          {error}
+        </Alert>
+      )}
+      <List>
+        {cities.map((e, i) => (
+          <ListItem button key={`${i}-${e.country}`} onClick={onClickCity}>
+            <Grid container justifyContent="center" alignItems="center">
+              <Grid item xs={12} md={8}>
+                <CityInfo city={e.city} country={e.country} />
+              </Grid>
+              <Grid item xs={12} md={4}>
+                {allWeather[`${i}-${e.country}`] ? (
+                  <Weather
+                    temp={allWeather[`${i}-${e.country}`]["temp"]}
+                    icon={allWeather[`${i}-${e.country}`]["icon"]}
+                  />
+                ) : loading ? (
+                  <LoadingCities />
+                ) : (
+                  "Sin datos"
+                )}
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={4}>
-              {allWeather[`${i}-${e.country}`] ? (
-                <Weather
-                  temp={allWeather[`${i}-${e.country}`]["temp"]}
-                  icon={allWeather[`${i}-${e.country}`]["icon"]}
-                />
-              ) : loading ? (
-                <LoadingCities />
-              ) : (
-                "Sin datos"
-              )}
-            </Grid>
-          </Grid>
-        </ListItem>
-      ))}
-    </List>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
