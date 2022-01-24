ff2b46a5333a414e12fd2d2b099f4c1dc57f1c75
Jesus Reyes <djrm1204@gmail.com>
2022-01-24 13:21:03 -0400

    Agregado custom hook a cityList

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,59 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Alert from "@mui/material/Alert";
 import Grid from "@mui/material/Grid";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Skeleton from "@mui/material/Skeleton";
-import ConvertUnits from "convert-units";
 
 import CityInfo from "../CityInfo/CityInfo";
 import Weather from "../Weather/Weather";
-import WeatherAxios from "../../api/WeatherAxios";
+import useCityList from "../../hooks/useCityList";
 
 const CityList = ({ cities, onClickCity }) => {
-  const [allWeather, setAllWeather] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    cities.forEach((c, i) => {
-      getInfoCity(c, i);
-    });
-  }, [cities]);
-
-  const getInfoCity = async ({ city, country, countryCode }, i) => {
-    try {
-      const resp = await WeatherAxios.get("/weather", {
-        params: {
-          q: `${city},${countryCode}`,
-        },
-      });
-      if (resp.data.main.temp) {
-        const propValue = {};
-        propValue.temp = Number(
-          ConvertUnits(resp.data.main.temp).from("K").to("C").toFixed(0)
-        );
-        propValue.icon = resp.data.weather[0].main.toLowerCase();
-        setAllWeather((current) => ({
-          ...current,
-          [`${i}-${country}`]: propValue,
-        }));
-        setLoading(false);
-      }
-    } catch (e) {
-      console.error(e);
-      setError((currentError) => {
-        if (e.response) {
-          return "Ha ocurrido un error en el servidor del clima";
-        } else if (e.request) {
-          return "Verifica tu conexion a internet";
-        } else {
-          return "Error al cargar datos";
-        }
-      });
-    }
-  };
+  const { allWeather, error, loading, setError } = useCityList(cities);
 
   return (
     <>
