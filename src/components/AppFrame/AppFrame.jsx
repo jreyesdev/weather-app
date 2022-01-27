073f5d225f3fd7ce70f3a23ae9422fb7f2e66102
Jesus Reyes <djrm1204@gmail.com>
2022-01-26 21:38:48 -0400

    Proveedor de contexto de lista de ciudades en tema principal

--- src/components/AppFrame/AppFrame.jsx
+++ src/components/AppFrame/AppFrame.jsx
@@ -9,33 +9,36 @@ import Link from "@mui/material/Link";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { WiDaySunny } from "react-icons/wi";
+import WeatherProvider from "../../contexts/WeatherContext";
 
 const AppFrame = ({ children }) => {
   return (
-    <Grid container justifyContent="center">
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton color="inherit" aria-label="menu">
-            <Link
-              component={LinkRouter}
-              to="/main"
-              color="inherit"
-              aria-label="menu"
-            >
-              <IconContext.Provider value={{ size: "2em" }}>
-                <WiDaySunny />
-              </IconContext.Provider>
-            </Link>
-          </IconButton>
-          <Typography variant="h6" color="inherit">
-            Weather App
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Grid item xs={12} sm={11} md={10} lg={8}>
-        {children}
+    <WeatherProvider>
+      <Grid container justifyContent="center">
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <IconButton color="inherit" aria-label="menu">
+              <Link
+                component={LinkRouter}
+                to="/main"
+                color="inherit"
+                aria-label="menu"
+              >
+                <IconContext.Provider value={{ size: "2em" }}>
+                  <WiDaySunny />
+                </IconContext.Provider>
+              </Link>
+            </IconButton>
+            <Typography variant="h6" color="inherit">
+              Weather App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Grid item xs={12} sm={11} md={10} lg={8}>
+          {children}
+        </Grid>
       </Grid>
-    </Grid>
+    </WeatherProvider>
   );
 };
 
